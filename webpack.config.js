e4a9212ded94dedf712205c0c45a4ebd1c4450bf
joshin-run <jahmsc@gmail.com>
2017-04-17 20:40:49 -0600

    Chore (init) Setup of files; webpack; test

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+//check for accuracy
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: "cheap-eval-source-map",
+  watch: true,
+};
